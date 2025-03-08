388c7f089b197e03647e38369961eefa10cbfd4b
ANIRBAN BOSE <129717625+anirbanbose401@users.noreply.github.com>
2025-03-08 21:54:27 +0530

    fix reload issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // ✅ Ensures correct asset paths
-  server: {
-    port: 3000
-  }
+  plugins: [],
 });
